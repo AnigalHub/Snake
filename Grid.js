a9923fed8ea335b7c5b2fee3ef598ef827e1cbd2
AnigalHub <anigal1997@gmail.com>
2021-03-03 23:10:38 +0300

    добавлен класс - квадрат

--- Grid.js
+++ Grid.js
@@ -1,6 +1,47 @@
 let canvas = document.getElementById("myCanvas");
 let context = canvas.getContext("2d");
 
+class Square {
+    constructor(top_indent,left_indent,side_of_square,color){
+        this.top_indent = top_indent;// отступ сверху - x  (x,y,w,h)
+        this.left_indent = left_indent;// отступ слева - y
+        this.side_of_square = side_of_square; // сторона квадрата - и w и h - т.к. это квадрат
+        this.color = color;
+    }
+    /*
+    CreateSquare(){
+        context.strokeStyle = "white";
+        context.fillStyle = this.color;
+        context.lineWidth = 2;
+        context.strokeRect((this.left_indent + (this.side_of_square)),(this.top_indent + (this.side_of_square)), this.side_of_square, this.side_of_square);
+        context.fillRect((this.left_indent + (this.side_of_square)),(this.top_indent+ (this.side_of_square)), this.side_of_square, this.side_of_square);
+    }
+    */
+}
+class Food extends Square{
+    constructor() {
+        super(40,180,50,"green");
+    }
+}
+class Grid {
+    constructor(width_field,height_field) {
+        this.width_field = width_field; // размер игрового поля (width_field) х (height_field)
+        this.height_field = height_field; // количество квадратов в строку и в столбик
+    }
+    CreateGrid(){
+        for (let rows = 0; rows < this.height_field; rows++){  // rows - количество строк (с квадратиками)
+            for (let columns = 0; columns < this.width_field; columns++) { // columns - количество столбцов (квадратов)
+                let squares = [rows][columns];
+                squares [rows][columns] = new Square(40,180,50,"green");
+            }
+        }
+    }
+
+}
+
+
+
+/*
  class Grid {
     constructor(top_indent,left_indent,side_of_square,width_field,height_field){
         this.top_indent = top_indent;// отступ сверху
@@ -22,31 +63,50 @@ let context = canvas.getContext("2d");
     }
 }
 
+let x,y;
+
 class Food extends Grid{
     constructor(){
         super(40,180,50,
             16,16);
     }
 
-    DrawSquare() {
+    DrawSquare(color) {
         function RandomInt(min, max) { //рандом квадрата на поле
             min = Math.ceil(min);
             max = Math.floor(max);
             return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
         }
 
-        let x = RandomInt(1,this.width_field); // номер по горизонтали(х)
+        x = RandomInt(1,this.width_field); // номер по горизонтали(х)
         console.log("номер по горизонтали(х) - " + x);
-        let y = RandomInt(1,this.height_field); // номер по вертикали (у)
+         y = RandomInt(1,this.height_field); // номер по вертикали (у)
         console.log("номер по вертикали (у) - " + y);
         
-        context.fillStyle = "red";
+        context.fillStyle = color;
         context.strokeRect((this.left_indent + ((x-1) * this.side_of_square)),(this.top_indent + ((y-1) * this.side_of_square)), this.side_of_square, this.side_of_square); // х,у,w
         context.fillRect((this.left_indent + ((x-1) * this.side_of_square)),(this.top_indent + ((y-1) * this.side_of_square)), this.side_of_square, this.side_of_square);
     }
+}
+class Snake extends Grid{
+     constructor() {
+         super(40,180,50,16,16);
+     }
+     DrawSnake() {
+         food.DrawSquare("white");
+         context.strokeRect((this.left_indent + ((x-1) * this.side_of_square)),(this.top_indent + ((y) * this.side_of_square)), this.side_of_square, this.side_of_square); // х,у,w
+         context.fillRect((this.left_indent + ((x-1) * this.side_of_square)),(this.top_indent + ((y) * this.side_of_square)), this.side_of_square, this.side_of_square);
+
+     }
 }
 
 let grid = new Grid(40,180,50,16,16);
 grid.DrawGrid();
 let food = new Food();
-food.DrawSquare();
+food.DrawSquare("red");
+
+
+let snake = new Snake();
+snake.DrawSnake();
+
+ */
