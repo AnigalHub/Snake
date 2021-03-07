7f5e18061b3ee77abdb7c00c4b4428de6610f55f
AnigalHub <anigal1997@gmail.com>
2021-03-08 00:25:07 +0300

    изменен конструктор - (лишь длина змеи), добавлен метод на создание

--- Snake.js
+++ Snake.js
@@ -1,8 +1,16 @@
 const  Square = require("./Square.js");
 
-class Snake extends Square{
-    constructor() {
-        super(170,2,50,"blue");
+class Snake{
+    constructor(lengthSnake) {
+       this.length = lengthSnake;
     }
+    CreateSnake(){
+        let snake = [];
+
+        for (let row = 0; row < this.length; row++){  // rows - количество строк (с квадратиками)
+              snake.push(new Square(120 + 50,2 + (row * 50),50,"blue"));
+            }
+        return snake;
+        }
 }
 module.exports = Snake ;
