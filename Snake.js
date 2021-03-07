ffcf3ff8ee2e9406d439e4942f3389efce6d1a2f
AnigalHub <anigal1997@gmail.com>
2021-03-08 02:40:47 +0300

    добавлен цвет как пареметр

--- Snake.js
+++ Snake.js
@@ -1,14 +1,15 @@
 const  Square = require("./Square.js");
 
 class Snake{
-    constructor(lengthSnake) {
+    constructor(lengthSnake,color) {
        this.length = lengthSnake;
+       this.color = color;
     }
     CreateSnake(){
         let snake = [];
 
         for (let row = 0; row < this.length; row++){  // rows - количество строк (с квадратиками)
-              snake.push(new Square(120 + 50,2 + (row * 50),50,"blue"));
+              snake.push(new Square(120 + 50,2 + (row * 50),50,this.color));
             }
         return snake;
         }
