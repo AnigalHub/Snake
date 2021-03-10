d99f033c65f41824de5c50afc178aad893f17579
AnigalHub <anigal1997@gmail.com>
2021-03-10 21:52:52 +0300

    добавлено движение змеи вниз - через массив

--- index.js
+++ index.js
@@ -13,9 +13,16 @@ function DrawSquare(square){
     context.strokeRect((square.left_indent + (square.side_of_square)),(square.top_indent + (square.side_of_square)), square.side_of_square, square.side_of_square);
     context.fillRect((square.left_indent + (square.side_of_square)),(square.top_indent+ (square.side_of_square)), square.side_of_square, square.side_of_square);
 }
+function DrawDefultSquare(square)  {
+context.strokeStyle = "white";
+context.fillStyle ="green";
+context.lineWidth = 2;
+context.strokeRect((square.left_indent + (square.side_of_square)),(square.top_indent + (square.side_of_square)), square.side_of_square, square.side_of_square);
+context.fillRect((square.left_indent + (square.side_of_square)),(square.top_indent+ (square.side_of_square)), square.side_of_square, square.side_of_square);
+}
 
 let grid = new Grid(16,16);
-let array = grid.CreateGrid();
+let array = grid.Squares;
 console.log(array);
 
 for (let square of array){
@@ -27,29 +34,57 @@ for (let square of array){
 DrawSquare(new Food());
 
 let snake = new Snake(4,"red");
-let arraySnake = snake.CreateSnake();
+let arraySnake = snake.Cells;
 
-let square = new Square(120,2,50,"")
 
 for (let square of arraySnake){
     console.log(square);
     DrawSquare(square);
 }
 
+
+
+
+
+document.addEventListener('keydown', function(event) {
+    if (event.code == 'KeyS') { //вниз - down
+        DrawDefultSquare(arraySnake.pop()); // удаление хвоста
+        console.log("голова");
+        console.log(arraySnake[0]);
+
+        console.log(arraySnake[0].top_indent);
+        let square = new Square(170,(arraySnake[0].top_indent)+50,50,"red");
+        arraySnake.unshift(square);
+        console.log(square);
+        DrawSquare(square);
+
+    }
+});
+
+
+/*
+let square = new Square(120,2,50,"")
+for (let square of arraySnake){
+    console.log(square);
+    DrawSquare(square);
+}
 document.addEventListener('keydown', function(event) {
     if (event.code == 'KeyZ') {
         console.log("работает");
         let snake2 = new Snake(4,"green");
-        let arraySnake2 = snake2.CreateSnake();
+        let arraySnake2 = snake2.Cells;
         for (let square of arraySnake2){
             console.log(square);
             DrawSquare(square);
         }
-
         context.translate(0, square.side_of_square);
         for (let square of arraySnake){
             console.log(square);
             DrawSquare(square);
         }
     }
+
 });
+*/
+
+
