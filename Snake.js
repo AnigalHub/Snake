2862e18d106155cdbcbd7fc5fbc90ba4acfc6d67
AnigalHub <anigal1997@gmail.com>
2021-03-11 21:52:59 +0300

    добавлен метод на сокращение (уменьшение хвоста)

--- Snake.js
+++ Snake.js
@@ -6,11 +6,15 @@ class Snake{
        this.color = color;
        this._cells = [];
         for (let row = 0; row < this.length; row++){  // rows - количество строк (с квадратиками)
-            this._cells.push(new Square(120 + 50,2 + (row * 50),50,this.color));
+            this._cells.unshift(new Square(120 + 50,2 + (row * 50),50,this.color));
         }
     }
     get Cells(){
         return this._cells;
     }
+    Shrink(){
+        return arraySnake.pop(); // удаление хвоста
+    };
+    
 }
 module.exports = Snake ;
