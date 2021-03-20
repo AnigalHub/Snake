a1e885b48853501fe9554c0a270318ea226e642c
AnigalHub <anigal1997@gmail.com>
2021-03-20 22:07:23 +0300

    добавлена функция рандома еды на поле

--- Food.js
+++ Food.js
@@ -1,8 +1,13 @@
 const  Square = require("./Square.js");
 
+
+function RandomNumber(min, max, step){
+    return step * Math.floor(Math.random() * (max - min) / step + min / step);
+}
+
 class Food extends Square{
-    constructor() {
-        super(120,0,50,"yellow");
+    constructor(width_field,height_field) {
+        super(120 + RandomNumber(50,50*width_field,50),RandomNumber(50,50*height_field,50),50,"yellow");
     }
 }
 module.exports = Food ;
