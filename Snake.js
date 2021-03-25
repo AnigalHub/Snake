48e0e5ab0754014760d667a16998492d30af045e
AnigalHub <anigal1997@gmail.com>
2021-03-25 17:55:14 +0300

    изменено местоположение

--- Snake.js
+++ Snake.js
@@ -7,7 +7,7 @@ class Snake{
        this.color = color;
        this._cells = [];
         for (let row = 0; row < this.length; row++){  // rows - количество строк (с квадратиками)
-            this._cells.push(new Square(120 + 50,50 + (row * 50),50,this.color));
+            this._cells.push(new Square(220 + 50,50 + (row * 50),50,this.color));
         }
     }
     get Cells(){
