0ea0adff364c4c7247d2323ece931fbbb9c04c0f
AnigalHub <anigal1997@gmail.com>
2021-03-31 22:51:59 +0300

    вынесен цвет

--- Food.js
+++ Food.js
@@ -6,8 +6,8 @@ function RandomNumber(min, max, step){
 }
 
 class Food extends Square{
-    constructor(width_field,height_field) {
-        super(120 + RandomNumber(0,50*width_field,50),RandomNumber(0,50*height_field,50),50,"yellow");
+    constructor(width_field,height_field,color) {
+        super(120 + RandomNumber(0,50*width_field,50),RandomNumber(0,50*height_field,50),50,color);
     }
 }
 module.exports = Food ;
--- Grid.js
+++ Grid.js
@@ -1,13 +1,14 @@
 const  Square = require("./Square.js");
 class Grid {
-    constructor(width_field,height_field) {
+    constructor(width_field,height_field,color) {
         this.width_field = width_field; // размер игрового поля (width_field) х (height_field)
         this.height_field = height_field; // количество квадратов в строку и в столбик
         this._squares = [];
+        this.color = color;
         for (let row = 0; row < this.height_field; row++){  // rows - количество строк (с квадратиками)
            this._rowSquare = [];
             for (let column = 0; column < this.width_field; column++) { // columns - количество столбцов (квадратов)
-                this._rowSquare.push(new Square(120 + (column * 50),(row * 50),50,"green"));
+                this._rowSquare.push(new Square(120 + (column * 50),(row * 50),50,this.color));
             }
             this._squares.push(this._rowSquare);
         }
@@ -15,6 +16,8 @@ class Grid {
     get Squares(){
         return this._squares;
     }
+
+
 }
 
 module.exports = Grid ;
